139875112130898a34d2fdacc175ee0552bafda5
Bandeira Magalhães <bandeiramagalhaesoficial@gmail.com>
2023-03-28 19:02:33 -0300

    :lipstick: feat: estilização da página home do projeto e mudança de fontes

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,28 @@
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 
 import { styles } from './styles';
+import { THEMES } from '../../THEMES';
 
-import Trash from '../../assets/trash.svg';
+import LogoTodo from '../../assets/todo.svg';
+import iconPlus from '../../assets/plus.svg';
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
-      <Trash />
+      <View style={styles.header}>
+        <LogoTodo />
+      </View>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder='Adicione uma nova tarefa'
+          placeholderTextColor={THEMES.gray300}
+        />
+
+        <iconPlus />
+        <TouchableOpacity style={styles.button}></TouchableOpacity>
+      </View>
     </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -5,7 +5,37 @@ import { THEMES } from '../../THEMES';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    
+
     backgroundColor: THEMES.gray600,
+  },
+  header: {
+    width: '100%',
+    
+    paddingVertical: 76,
+
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    backgroundColor: THEMES.gray700,
+  },
+  form: {
+    width: '100%',
+
+    flexDirection: 'row',
+
+    backgroundColor: 'THEMES.blueDark',
+  },
+  input: {
+    flex: 1,
+
+    height: 56,
+
+    paddingHorizontal: 26,
+
+    backgroundColor: THEMES.gray500,
+  },
+  button: {
+    width: 56,
+    height: 56,
   }
 });
