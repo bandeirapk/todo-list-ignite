5db2172547539eec5d9bf3007d38fd0446764b55
Bandeira Magalhães <bandeiramagalhaesoficial@gmail.com>
2023-03-28 21:59:16 -0300

    :lipstick: feat: estilização do componente home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,28 +1,87 @@
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Image,
+  Keyboard,
+} from 'react-native';
 
 import { styles } from './styles';
-import { THEMES } from '../../THEMES';
 
 import LogoTodo from '../../assets/todo.svg';
-import iconPlus from '../../assets/plus.svg';
+import PlusIcon from '../../assets/plus.svg';
+import { THEMES } from '../../THEMES';
 
 export function Home() {
+  const [isFocused, setIsFocused] = useState(false);
+
+  function handleFocus() {
+    setIsFocused(true);
+  }
+
+  function handleBlur() {
+    setIsFocused(false);
+  }
+
+  function handleBlurWithKeyboard() {
+    Keyboard.dismiss();
+  }
+
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <LogoTodo />
-      </View>
+    <TouchableWithoutFeedback onPress={handleBlurWithKeyboard}>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <LogoTodo />
+        </View>
 
-      <View style={styles.form}>
-        <TextInput
-          style={styles.input}
-          placeholder='Adicione uma nova tarefa'
-          placeholderTextColor={THEMES.gray300}
-        />
+        <View style={styles.form}>
+          <TextInput
+            style={[
+              styles.input,
+              { borderColor: isFocused ? THEMES.purpleDark : THEMES.gray100 },
+            ]}
+            onFocus={() => handleFocus()}
+            onBlur={() => handleBlur()}
+            placeholder='Adicione uma nova tarefa'
+          />
 
-        <iconPlus />
-        <TouchableOpacity style={styles.button}></TouchableOpacity>
+          <TouchableOpacity style={styles.button} activeOpacity={0.6}>
+            <PlusIcon />
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.tasks}>
+          <View style={styles.info}>
+            <View style={styles.created}>
+              <Text style={styles.taskCreated}>Criadas</Text>
+              <Text style={styles.tasksCounter}>0</Text>
+            </View>
+
+            <View style={styles.done}>
+              <Text style={styles.taskCompleted}>Concluídas</Text>
+              <Text style={styles.tasksCounter}>0</Text>
+            </View>
+          </View>
+
+          <View style={styles.tasksList}>
+            <Image
+              source={require('../../assets/clipboardIcon.png')}
+              style={styles.clipboardIcon}
+            />
+            <View>
+              <Text style={styles.textEmptyOne}>
+                Você ainda não tem tarefas cadastradas
+              </Text>
+              <Text style={styles.textEmptyTwo}>
+                Crie tarefas e organize seus itens a fazer
+              </Text>
+            </View>
+          </View>
+        </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -22,6 +22,11 @@ export const styles = StyleSheet.create({
     width: '100%',
 
     flexDirection: 'row',
+    gap: 6,
+
+    paddingHorizontal: 24,
+
+    marginVertical: -28,
 
     backgroundColor: 'THEMES.blueDark',
   },
@@ -30,12 +35,99 @@ export const styles = StyleSheet.create({
 
     height: 56,
 
-    paddingHorizontal: 26,
+    paddingHorizontal: 16,
+    
+    borderRadius: 6,
+    borderWidth: 1,
+
+    fontSize: 16,
+    lineHeight: 24,
+
+    color: THEMES.gray700,
 
     backgroundColor: THEMES.gray500,
   },
   button: {
     width: 56,
     height: 56,
+
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    borderRadius: 6,
+
+    backgroundColor: THEMES.blueDark,
+  },
+  tasks: {
+    marginTop: 60,
+    marginHorizontal: 24,
+  },
+  info: {
+    flexDirection: 'row',
+  },
+  created: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+
+    marginLeft: 2
+  },
+  done: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+
+    marginRight: 24,
+  },
+  taskCreated: {    
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: THEMES.blue,
+  },
+  taskCompleted: {
+    fontWeight: 'bold',
+    fontSize: 16,
+
+    marginLeft: 88,
+
+    color: THEMES.purple,
+  },
+  tasksCounter: {
+    paddingVertical: 2,
+    paddingHorizontal: 8,
+
+    borderRadius: 12,
+
+    backgroundColor: THEMES.gray400,
+  },
+  tasksList: {
+    marginTop: 24,
+
+    alignItems: 'center',
+    gap: 16,
+  },
+  clipboardIcon: {
+    marginTop: 48,
+  },
+  textEmptyOne: {
+    marginTop: 12,
+
+    fontWeight: 'bold',
+    fontSize: 16,
+
+    lineHeight: 20,
+    textAlign: 'center',
+    
+    color: THEMES.gray300,
+  },
+  textEmptyTwo: {
+    fontSize: 14,
+
+    lineHeight: 20,
+    textAlign: 'center',
+    
+    color: THEMES.gray300,
   }
 });
